346d64b598e6e6946c174a1c7f6e0ce6f77bc66f
SatoTaisei <t.sei43rm@gmail.com>
2022-01-06 16:42:08 +0900

    ♻️ TailwindCSSの適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 const App = () => {
   return (
     <>
-      <header>TODO</header>
+      <header className="text-2xl text-gray-50 font-bold text-center leading-none bg-gray text-white w-full pt-14 pb-2 box-border">
+        TODO
+      </header>
       <main>TODO Display</main>
-      <footer></footer>
+      <footer className="w-full bg-gray h-8"></footer>
     </>
   );
 };
