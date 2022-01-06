5ea64938f3684b7356560143ad83264bcf6e3088
SatoTaisei <t.sei43rm@gmail.com>
2022-01-06 16:41:42 +0900

    💄 カラーパレットの更新

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+const { colors } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      gray: "#333333",
+      white: "#e9e9e9",
+    },
+    plugins: [],
   },
-  plugins: [],
 };
