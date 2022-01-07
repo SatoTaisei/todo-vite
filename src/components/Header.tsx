1858c24833f3ed6962123ea386b04fda1d5b6934
SatoTaisei <t.sei43rm@gmail.com>
2022-01-07 20:19:51 +0900

    💄 ヘッダーのレスポンシブ対応

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export const Header: React.VFC = () => {
   return (
-    <header className="text-2xl text-gray-50 font-bold text-center leading-none bg-gray text-white w-full pt-14 pb-2 box-border">
+    <header className="text-2xl text-gray-50 font-bold text-center leading-none bg-gray text-white w-full pt-14 pb-2 box-border md:text-3xl md:py-4 md:pl-6 md:text-left">
       TODO
     </header>
   );
