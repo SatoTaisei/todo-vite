9e290d3becf8db354e5babbb327f82f3e91ad8da
SatoTaisei <t.sei43rm@gmail.com>
2022-02-27 22:33:17 +0900

    🎨 Todoの追加・表示・削除機能の追加

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,71 @@
-import { Footer } from "@/components/Footer";
+import { ChangeEvent, useState } from "react";
 import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 
-const App = () => {
+export const App: React.VFC = () => {
+  const [text, setText] = useState<string>("");
+  const [todos, setTodos] = useState<string[]>([]);
+
   return (
     <>
       <Header />
-      <main>Todo Item</main>
+      <main>
+        <input
+          type="text"
+          placeholder="TODOを入力"
+          className="border-4 border-solid border-gray rounded-sm placeholder-white"
+          value={text}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => {
+            setText(e.target.value);
+          }}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && text !== "") {
+              const newTodos = [...todos];
+              newTodos.push(text);
+              setTodos([...newTodos]);
+              setText("");
+            }
+          }}
+        />
+        <button
+          type="button"
+          className="w-8 border-4 border-solid border-gray rounded-sm"
+          onClick={() => {
+            if (text === "") return;
+            const newTodos = [...todos];
+            newTodos.push(text);
+            setTodos([...newTodos]);
+            setText("");
+          }}
+        >
+          {/* 追加ボタン */}
+          {"\u002B"}
+        </button>
+        {todos.length !== 0 ? (
+          <ul>
+            {todos.map((todo: string, index: number) => (
+              <li key={index} className="text-base">
+                <button></button>
+                <span>{todo}</span>
+                <button
+                  className="w-8 border-4 border-solid border-gray rounded-sm"
+                  onClick={() => {
+                    const newTodos = [...todos];
+                    newTodos.splice(index, 1);
+                    setTodos(newTodos);
+                  }}
+                >
+                  {/* 削除ボタン */}
+                  {"\u2716"}
+                </button>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className="text-gray">TODOを入力してください。</p>
+        )}
+      </main>
       <Footer />
     </>
   );
 };
-
-export default App;
